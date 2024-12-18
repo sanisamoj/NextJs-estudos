573dc7ea9f2901f02747b862aab8418ccf335337
João Mário <marioartec39@gmail.com>
2024-12-17 23:50:41 -0300

    calendar

--- app/ui/calendar/CustomCalendar.tsx
+++ app/ui/calendar/CustomCalendar.tsx
@@ -1,116 +1,139 @@
 'use client'
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDateContext } from "../DateContext";
 
 const CustomCalendar = () => {
-  const [currentDate, setCurrentDate] = useState(new Date())
-  const [selectedDate, setSelectedDate] = useState(new Date())
-  const [view, setView] = useState("month")
+    const { selectedDate, setSelectedDate } = useDateContext();
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [view, setView] = useState("month");
 
-  // Função para gerar os dias do mês
-  const generateDays = (date: Date) => {
-    const year = date.getFullYear();
-    const month = date.getMonth();
+    useEffect(() => {
+        setCurrentDate(selectedDate); // Atualiza o currentDate quando selectedDate muda
+    }, [selectedDate]);
 
-    // Primeiro e último dia do mês
-    const firstDay = new Date(year, month, 1);
-    const lastDay = new Date(year, month + 1, 0);
+    // Função para adicionar evento
+    const handleAddEvent = () => {
+        // Aqui você pode implementar a lógica para adicionar um evento
+        alert("Adicionar evento clicado!"); // Exemplo de ação
+    };
 
-    // Dias antes do primeiro dia do mês (para completar a semana)
-    const daysBefore = firstDay.getDay();
+    // Função para gerar os dias do mês
+    const generateDays = (date: Date) => {
+        const year = date.getFullYear();
+        const month = date.getMonth();
 
-    // Todos os dias do mês
-    const daysInMonth = Array.from(
-      { length: lastDay.getDate() },
-      (_, i) => new Date(year, month, i + 1)
-    );
+        // Primeiro e último dia do mês
+        const firstDay = new Date(year, month, 1);
+        const lastDay = new Date(year, month + 1, 0);
 
-    return daysInMonth;
-  };
+        // Dias antes do primeiro dia do mês (para completar a semana)
+        const daysBefore = firstDay.getDay();
 
-  // Gerar os dias da semana
-  const generateWeekDays = (date: Date) => {
-    const startOfWeek = new Date(date);
-    startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay()); // Ajusta para o início da semana (Domingo)
-    return Array.from({ length: 7 }, (_, i) => {
-      const day = new Date(startOfWeek);
-      day.setDate(startOfWeek.getDate() + i);
-      return day;
-    });
-  };
+        // Todos os dias do mês
+        const daysInMonth = Array.from(
+            { length: lastDay.getDate() },
+            (_, i) => new Date(year, month, i + 1)
+        );
 
-  const goToPreviousMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
-  };
+        return daysInMonth;
+    };
 
-  const goToNextMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
-  };
+    // Gerar os dias da semana
+    const generateWeekDays = (date: Date) => {
+        const startOfWeek = new Date(date);
+        startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay()); // Ajusta para o início da semana (Domingo)
+        return Array.from({ length: 7 }, (_, i) => {
+            const day = new Date(startOfWeek);
+            day.setDate(startOfWeek.getDate() + i);
+            return day;
+        });
+    };
 
-  const toggleView = () => {
-    setView(view === "month" ? "week" : "month");
-  };
+    const goToPreviousMonth = () => {
+        setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
+    };
 
-  return (
-    <div className="flex-1 bg-white shadow-lg rounded-lg overflow-hidden">
-      <header className="flex justify-between items-center bg-blue-500 text-white p-4">
-        <button onClick={goToPreviousMonth} className="text-lg font-bold hover:bg-blue-600 px-2 py-1 rounded">&lt;</button>
-        <h2 className="text-xl font-semibold">
-          {view === "month"
-            ? currentDate.toLocaleString("default", { month: "long", year: "numeric" })
-            : currentDate.toLocaleString("default", { month: "long", year: "numeric" }) + " - Semana"}
-        </h2>
-        <button onClick={goToNextMonth} className="text-lg font-bold hover:bg-blue-600 px-2 py-1 rounded">&gt;</button>
-      </header>
+    const goToNextMonth = () => {
+        setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
+    };
 
-      <div className="flex justify-between p-4">
-        <button onClick={toggleView} className="bg-blue-500 text-white px-4 py-2 rounded">
-          {view === "month" ? "Ver Semanal" : "Ver Mensal"}
-        </button>
-      </div>
+    const toggleView = () => {
+        setView(view === "month" ? "week" : "month");
+    };
 
-      <div className="overflow-auto" style={{ height: '70vh' }}>
-        {view === "week" && (
-          <div className="grid grid-cols-8 text-center font-semibold border-b">
-            {generateWeekDays(currentDate).map((day) => (
-              <div key={day.toString()} className="py-2 border-b">
-                {day.toLocaleDateString("default", { weekday: "short" })}
-              </div>
-            ))}
-          </div>
-        )}
+    const handleDateClick = (day: Date) => {
+        setSelectedDate(day);
+        setCurrentDate(day); // Atualiza o currentDate no MiniCalendar
+    }
 
-        <div className={`grid ${view === "week" ? "grid-cols-8" : "grid-cols-7"} gap-1 p-2`}>
-          {view === "week" ? (
-            // Renderizando os horários como cards na lateral
-            Array.from({ length: 24 }, (_, hour) => (
-              <React.Fragment key={hour}>
-                <div className="border h-16 p-2 text-center font-bold">
-                  {hour.toString().padStart(2, '0')}:00
+    return (
+        <div className="flex-1 bg-white shadow-lg rounded-lg overflow-hidden">
+            <header className="flex justify-between items-center bg-blue-500 text-white p-4">
+                <button onClick={goToPreviousMonth} className="text-lg font-bold hover:bg-blue-600 px-2 py-1 rounded">&lt;</button>
+                <h2 className="text-xl font-semibold">
+                    {view === "month"
+                        ? currentDate.toLocaleString("default", { month: "long", year: "numeric" })
+                        : currentDate.toLocaleString("default", { month: "long", year: "numeric" }) + " - Semana"}
+                </h2>
+                <button onClick={goToNextMonth} className="text-lg font-bold hover:bg-blue-600 px-2 py-1 rounded">&gt;</button>
+            </header>
+
+            <div className="flex justify-between items-center p-4">
+                <button></button>
+            </div>
+
+            <div className="flex justify-between p-4">
+                <button onClick={toggleView} className="bg-blue-500 text-white px-4 py-2 rounded">
+                    {view === "month" ? "Ver Semanal" : "Ver Mensal"}
+                </button>
+                <button
+                    onClick={handleAddEvent}
+                    className="bg-green-500 text-white p-2 rounded hover:bg-green-600"
+                    title="Adicionar evento">
+                    Adicionar evento
+                    <span className="text-lg p-1">+</span>
+                </button>
+            </div>
+
+            <div className="overflow-auto" style={{ height: '70vh' }}>
+                {view === "week" && (
+                    <div className="grid grid-cols-8 text-center font-semibold border-b">
+                        {generateWeekDays(currentDate).map((day) => (
+                            <div key={day.toString()} className="py-2 border-b">
+                                {day.toLocaleDateString("default", { weekday: "short" })}
+                            </div>
+                        ))}
+                    </div>
+                )}
+
+                <div className={`grid ${view === "week" ? "grid-cols-8" : "grid-cols-7"} gap-1 p-2`}>
+                    {view === "week" ? (
+                        Array.from({ length: 24 }, (_, hour) => (
+                            <React.Fragment key={hour}>
+                                <div className="border h-16 p-2 text-center font-bold">
+                                    {hour.toString().padStart(2, '0')}:00
+                                </div>
+                                {generateWeekDays(currentDate).map((day) => (
+                                    <div key={day.toString()} className="border h-16 p-2 rounded-lg cursor-pointer hover:bg-blue-100 transition duration-200">
+                                        <div className="mt-2 text-sm text-gray-600">Clique para adicionar</div>
+                                    </div>
+                                ))}
+                            </React.Fragment>
+                        ))
+                    ) : (
+                        generateDays(currentDate).map((day, index) => (
+                            <div
+                                key={index}
+                                onClick={() => handleDateClick(day)}
+                                className={`border h-32 p-2 rounded-lg cursor-pointer hover:bg-blue-100 transition duration-200`}>
+                                <div className="font-bold">{day.getDate()}</div>
+                            </div>
+                        ))
+                    )}
                 </div>
-                {generateWeekDays(currentDate).map((day) => (
-                  <div key={day.toString()} className="border h-16 p-2 rounded-lg cursor-pointer hover:bg-blue-100 transition duration-200">
-                    <div className="mt-2 text-sm text-gray-600">Clique para adicionar</div>
-                  </div>
-                ))}
-              </React.Fragment>
-            ))
-          ) : (
-            // Renderizando os dias do mês
-            generateDays(currentDate).map((day, index) => (
-              <div key={index} className={`border h-32 p-2 rounded-lg cursor-pointer hover:bg-blue-100 transition duration-200`}>
-                <div className="font-bold">{day.getDate()}</div>
-                <div className="mt-2 text-sm text-gray-600">Clique para adicionar</div>
-              </div>
-            ))
-          )}
+            </div>
         </div>
-      </div>
-
-      {/* Detalhes do Dia */}
-      {/* ... código existente para detalhes do dia ... */}
-    </div>
-  );
+    );
 };
-
 
 export default CustomCalendar;
--- app/ui/calendar/MiniCalendar.tsx
+++ app/ui/calendar/MiniCalendar.tsx
@@ -1,60 +1,73 @@
 'use client'
-import React, { useState } from "react";
+import React, { useContext, useState, useEffect } from "react"
+import { useDateContext } from "../DateContext"
+import capitalizeFirstLetter from "../utils/capitelizeFirstLetter"
 
 const MiniCalendar = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState(new Date())
+  const { selectedDate, setSelectedDate } = useDateContext()
+  const [view, setView] = useState("month")
+
+  useEffect(() => {
+    // Atualiza o currentDate quando selectedDate muda
+    setCurrentDate(selectedDate)
+  }, [selectedDate])
 
   // Função para gerar os dias do mês
   const generateDays = (date: Date) => {
-    const year = date.getFullYear();
-    const month = date.getMonth();
+    const year = date.getFullYear()
+    const month = date.getMonth()
 
     // Primeiro e último dia do mês
-    const firstDay = new Date(year, month, 1);
-    const lastDay = new Date(year, month + 1, 0);
+    const firstDay = new Date(year, month, 1)
+    const lastDay = new Date(year, month + 1, 0)
 
     // Dias antes do primeiro dia do mês (para completar a semana)
-    const daysBefore = firstDay.getDay();
+    const daysBefore = firstDay.getDay()
 
     // Todos os dias do mês
     const daysInMonth = Array.from(
       { length: lastDay.getDate() },
       (_, i) => new Date(year, month, i + 1)
-    );
+    )
 
     // Dias extras antes e depois
     const daysBeforeMonth = Array.from({ length: daysBefore }, (_, i) => {
-      const day = new Date(year, month, 0);
-      day.setDate(day.getDate() - (daysBefore - 1 - i));
-      return day;
-    });
+      const day = new Date(year, month, 0)
+      day.setDate(day.getDate() - (daysBefore - 1 - i))
+      return day
+    })
 
-    const totalDays = [...daysBeforeMonth, ...daysInMonth];
+    const totalDays = [...daysBeforeMonth, ...daysInMonth]
 
     // Completar até 6 semanas no total
-    const totalWeeks = Math.ceil(totalDays.length / 7);
+    const totalWeeks = Math.ceil(totalDays.length / 7)
     const daysAfterMonth = Array.from(
       { length: totalWeeks * 7 - totalDays.length },
       (_, i) => {
-        const day = new Date(lastDay);
-        day.setDate(lastDay.getDate() + i + 1);
-        return day;
+        const day = new Date(lastDay)
+        day.setDate(lastDay.getDate() + i + 1)
+        return day
       }
-    );
+    )
 
-    return [...totalDays, ...daysAfterMonth];
-  };
+    return [...totalDays, ...daysAfterMonth]
+  }
 
-  const days = generateDays(currentDate);
+  const days = generateDays(currentDate)
 
   const goToPreviousMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
-  };
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1))
+  }
 
   const goToNextMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
-  };
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1))
+  }
+
+  const handleDateClick = (day: Date) => {
+    setSelectedDate(day)
+    setCurrentDate(day) // Atualiza o currentDate no MiniCalendar
+  }
 
   return (
     <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
@@ -65,8 +78,8 @@ const MiniCalendar = () => {
         >
           &lt;
         </button>
-        <h2 className="text-xl font-semibold">
-          {currentDate.toLocaleString("default", { month: "long", year: "numeric" })}
+        <h2 className="text-xl font-semibold w-24 text-center"> {/* Defina uma largura fixa */}
+          {capitalizeFirstLetter(currentDate.toLocaleString("default", { month: "long", year: "numeric" }))}
         </h2>
         <button
           onClick={goToNextMonth}
@@ -77,7 +90,7 @@ const MiniCalendar = () => {
       </header>
 
       <div className="grid grid-cols-7 text-center font-semibold border-b">
-        {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day) => (
+        {["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"].map((day) => (
           <div key={day} className="py-2">
             {day}
           </div>
@@ -88,16 +101,15 @@ const MiniCalendar = () => {
         {days.map((day, index) => (
           <button
             key={index}
-            onClick={() => setSelectedDate(day as any)} 
-            className={`py-2 rounded-lg hover:bg-blue-100 transition duration-200 ${
-              day.getMonth() !== currentDate.getMonth()
-                ? "text-gray-400"
-                : day.toDateString() === new Date().toDateString()
+            onClick={() => handleDateClick(day)}
+            className={`py-2 rounded-lg hover:bg-blue-100 transition duration-200 ${day.getMonth() !== currentDate.getMonth()
+              ? "text-gray-400"
+              : day.toDateString() === new Date().toDateString()
                 ? "bg-blue-500 text-white"
                 : selectedDate && day.toDateString() === selectedDate.toDateString()
-                ? "bg-blue-300"
-                : ""
-            }`}
+                  ? "bg-blue-300"
+                  : ""
+              }`}
           >
             {day.getDate()}
           </button>
@@ -107,13 +119,13 @@ const MiniCalendar = () => {
       {selectedDate && (
         <div className="p-4 border-t">
           <h3 className="text-lg font-semibold">Detalhes do Dia</h3>
-          <p className="mt-2 text-gray-600">
+          <p className="mt-2 text-gray-600 truncate" style={{ maxWidth: '200px' }}> {/* Use truncate para evitar quebra de layout */}
             Data Selecionada: {selectedDate.toDateString()}
           </p>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default MiniCalendar;
+export default MiniCalendar
